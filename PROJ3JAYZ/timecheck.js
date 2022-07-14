001b26295d9416db9f55f9d1cc512922208d957f
Jack <37642239+jackintoshplus@users.noreply.github.com>
2022-07-14 01:16:19 -0700

    Upload Project 3 skeleton

---
+++ PROJ3JAYZ/timecheck.js
@@ -0,0 +1,19 @@
+/*  timecheck.js
+    Jack Herbert 7/14/2022
+
+    Gets the current time in order to check if it is
+    currently 4:44 - aka JAY-Z Time.
+*/
+
+var time = new Date();
+
+console.log("Time: " + time.getHours + ":" + time.getMinutes);
+
+if((time.getHours == 4 || 14) && (time.getMinute === 44))
+{
+    console.log("It is 4:44!");
+}
+else
+{
+    console.log("It is not 4:44...");
+}
